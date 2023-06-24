9ef36ef32f6a33b9035ee024a8a6453416e73a2e
User <crowtorakawa@gmail.com>
2023-06-24 23:07:59 +0800

    save

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs =  require('express-handlebars')
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set ('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/',(req, res) => {
-    res.send(`this is my first Express Web App`)
+    res.render('index')
   })
+
+app.get('/shorts/:url',(req,res) => {
   
+})
+
+app.post('/shorts' ,(req,res) => {
+  
+})
 app.listen(port, () => {
     console.log(`The server is listening on http://hostname:${port}`)
   })
 
-app.get('',(req, res) => {
+
+// app.get('',(req, res) => {
   
-})
+// })
