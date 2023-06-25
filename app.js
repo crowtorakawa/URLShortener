5afd26fce5db7e7e1cb1446247add6a62358814b
User <crowtorakawa@gmail.com>
2023-06-25 15:54:03 +0800

    generate path Code suceesful

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const generatePathCode = require('./generate_trans_code')
+
+// handlebars
 const exphbs =  require('express-handlebars')
-
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set ('view engine', 'handlebars')
 
+// body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))    備用測試
+
+// css js 資料夾位置
 app.use(express.static('public'))
+
 
 app.get('/',(req, res) => {
     res.render('index')
@@ -19,14 +27,16 @@ app.get('/shorts/:url',(req,res) => {
   
 })
 
-app.post('/shorts' ,(req,res) => {
-  
+app.post('/',(req,res) => {
+  const urlSource = req.body
+  const pathCode = generatePathCode()
+  console.log('req.body', urlSource)
+  console.log('pathCode', pathCode)
+  res.render('index',{ pathCode: pathCode})
 })
+
 app.listen(port, () => {
     console.log(`The server is listening on http://hostname:${port}`)
   })
 
 
-// app.get('',(req, res) => {
-  
-// })
