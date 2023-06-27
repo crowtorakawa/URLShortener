01ce2eff47b4ea6e3da372d00d6a666fda5074d2
crowtorakawa@gmail.com~ <crowtorakawa@gmail.com~>
2023-06-27 13:59:09 +0800

    save ok

--- routes/modules/shorts.js
+++ routes/modules/shorts.js
@@ -1,27 +1,29 @@
+// 連線用const
 const express = require('express')
+
 const router = express.Router()
 
 const urlConvert = require('../../models/urlConvert')
 
-// const generatePathCode = require('./generate_trans_code')
 
+//搜尋對應短網址路由
 router.get('/:url',(req, res) =>{
-    const urls = req.params.url
-    console.log(urls)
-    return urlConvert.find()
-        .lean()
-        .then(Convert => {
-          const findURL = Convert.filter(
-            ({ shortCode }) =>
-            shortCode.includes(urls)
-          )
-          console.log(findURL[0].source)
-          // 有找到
-          res.redirect(`${findURL[0].source}`)
-        //    res.redirect('/')
-        })
-        
-        .catch(error => console.log(error))
+  const urls = req.params.url
+  console.log(urls)
+  // 根據五碼搜尋來源網址
+  return urlConvert.find()
+    .lean()
+    .then(Convert => {
+      const findURL = Convert.filter(
+        ({ shortCode }) =>
+          shortCode.includes(urls)
+      )
+      console.log(findURL[0].source)
+      // 轉跳至資料庫內的來源網址    
+      res.redirect(`${findURL[0].source}`)      
+    })
+    .catch(error => console.log(error))
 })
-  
+
+//exports 給app.js  
 module.exports = router
