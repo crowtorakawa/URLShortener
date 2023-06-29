86b867bd2ba3af19fc7f358b02cea859727dc7dc
User <crowtorakawa@gmail.com>
2023-06-29 16:09:03 +0800

    fix

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,25 +13,24 @@ router.post('/', (req, res) => {
   // 先判斷有沒有內容在裡面，或有沒有符合url合法性
   if (urlSource.Unshort.length === 0 || URLlValid(urlSource.Unshort) !== true) {
     return res.render('index', { error: '請重新輸入' })
-  } else {
-    // 有內容就先從資料庫看看有無紀錄
-    urlConvert.find({ source: urlSource.Unshort })
-      .lean()
-      .then(Convert => {
-        if (Convert.length !== 0) {
-          return res.render('index', { pathCode: Convert[0].shortCode })
-        }
-        // 沒有從資料庫找到相同的網址，產生新的五碼
-        const pathFiveCode = generatePathCode()
-        // news = 新的網址 + 新的五碼
-        const news = { source: urlSource.Unshort, shortCode: pathFiveCode }
-        // 把新的資料建立在database
-        return urlConvert.create(news)
-          .then(() => res.render('index', { pathCode: pathFiveCode }))
-          .catch(err => console.log(err))
-      })
-      .catch(error => console.log(error))
   }
+  // 有內容就先從資料庫看看有無紀錄
+  urlConvert.find({ source: urlSource.Unshort })
+    .lean()
+    .then(Convert => {
+      if (Convert.length !== 0) {
+        return res.render('index', { pathCode: Convert[0].shortCode })
+      }
+      // 沒有從資料庫找到相同的網址，產生新的五碼
+      const pathFiveCode = generatePathCode()
+      // news = 新的網址 + 新的五碼
+      const news = { source: urlSource.Unshort, shortCode: pathFiveCode }
+      // 把新的資料建立在database
+      return urlConvert.create(news)
+        .then(() => res.render('index', { pathCode: pathFiveCode }))
+        .catch(err => console.log(err))
+    })
+    .catch(error => console.log(error))
 })
 // 首頁
 router.get('/', (req, res) => {
